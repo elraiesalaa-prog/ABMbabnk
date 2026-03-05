96ae92f9a471e17d15ba23d81a70d1f848513326
elraiesalaa-prog <elraiesalaa@gmail.com>
2026-03-05 16:50:36 +0200

    Update bank.js

--- bank.js
+++ bank.js
@@ -322,14 +322,18 @@ async function downloadPDF() {
 
   document.getElementById("pdfBalance").innerText =
     "الرصيد الحالي: " + document.getElementById("balance").innerText;
+  document.getElementById("pdfDate").innerText =
+  "تاريخ الإصدار: " + new Date().toLocaleString("ar-EG");
 
   // نسخ العمليات
   const rows = document.querySelectorAll("#transactionsBody tr");
   pdfBody.innerHTML = "";
 
   rows.forEach(row => {
-    pdfBody.appendChild(row.cloneNode(true));
-  });
+  const clonedRow = row.cloneNode(true);
+  clonedRow.style.pageBreakInside = "avoid";
+  pdfBody.appendChild(clonedRow);
+});
 
   // إظهار منطقة الطباعة مؤقتاً
   printArea.style.display = "block";
@@ -347,70 +351,13 @@ async function downloadPDF() {
       format: 'a4',
       orientation: 'portrait'
     }
+    
   };
 
   await html2pdf().set(opt).from(printArea).save();
 
   // إخفاؤها مرة أخرى
   printArea.style.display = "none";
-}
-// ================= فلترة =================
-
-async function filterTransactions(){
-
-  const from = document.getElementById("fromDate").value;
-  const to = document.getElementById("toDate").value;
-
-  const { data: userData } = await supabase.auth.getUser();
-  const user = userData.user;
-
-  let query = supabase
-    .from("transactions")
-    .select("*")
-    .eq("user_id", user.id)
-    .order("created_at", {ascending:false});
-
-  if(from){
-    query = query.gte("created_at", from);
-  }
-
-  if(to){
-    query = query.lte("created_at", to + "T23:59:59");
-  }
-
-  const { data, error } = await query;
-
-  if(error){
-    console.log(error);
-    return;
-  }
-
-  const tbody = document.getElementById("transactionsBody");
-  tbody.innerHTML="";
-
-  data.forEach(tx=>{
-
-    const row=document.createElement("tr");
-
-    const date=new Date(tx.created_at).toLocaleString("ar-EG");
-
-    let typeText="";
-
-    if(tx.type==="deposit") typeText="إيداع";
-    else if(tx.type==="withdraw") typeText="سحب";
-    else if(tx.type==="transfer") typeText="تحويل";
-
-    row.innerHTML=`
-      <td>${date}</td>
-      <td>${typeText}</td>
-      <td>${tx.amount || ""}</td>
-      <td>${tx.description || ""}</td>
-    `;
-
-    tbody.appendChild(row);
-
-  });
-
 }
 // ================= خروج =================
 async function logout(){
@@ -444,6 +391,7 @@ function showLogin(){
   document.getElementById("registerView").style.display = "none";
   document.getElementById("loginView").style.display = "block";
 }
+
 
 
 
