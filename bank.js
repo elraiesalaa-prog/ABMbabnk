83c4cc9064c8a7bc9427075f662e2dab38b57d71
elraiesalaa-prog <elraiesalaa@gmail.com>
2026-03-26 10:39:38 +0200

    Update bank.js

--- bank.js
+++ bank.js
@@ -270,7 +270,7 @@
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
-<script src="Amiri-Regular.js"></script>
+   <script src="Amiri-Regular.js"></script>
 <script src="bank.js"></script>
 
 </body>
